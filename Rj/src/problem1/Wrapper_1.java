52499b028aa3b2794117c347c6d9dbc61995d872
amit0426 <amitkumar9761785380@gmail.com>
2022-09-12 08:48:15 +0530

    changes

---
+++ Rj/src/problem1/Wrapper_1.java
@@ -0,0 +1,30 @@
+package problem1;
+
+
+
+import java.util.ArrayList;
+
+public class Wrapper_1 {
+	
+	public static void main(String[] args) {
+		
+//		int a = 20;
+//		Integer i = Integer.valueOf(a);
+//		Integer j = a;
+//		System.out.println(i+" "+a+" "+j);
+		
+		
+//		Integer a =new Integer(3); //integer to int
+//		int i = a.intValue(); //unboxing converting integer to int
+//		int j = a; // auto boxing 
+//		System.out.println(a+" "+" "+i+" "+j);
+			
+		char ch = 'a';
+		Character a = ch;
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(25);
+		System.out.println(arrayList.get(0));
+		
+	}
+
+}
